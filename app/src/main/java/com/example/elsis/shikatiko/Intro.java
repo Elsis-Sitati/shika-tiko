01d2a69a4e7426e2fd964f5cd09196b4dc4078d7
Elsis-Sitati <sitatielsis@gmail.com>
2016-03-08 15:03:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/elsis/shikatiko/Intro.java
@@ -0,0 +1,52 @@
+package com.example.elsis.shikatiko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class Intro extends IntroActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        /* Enable/disable fullscreen */
+        setFullscreen(true);
+
+        super.onCreate(savedInstanceState);
+
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.title_1)
+                .description(R.string.description_1)
+
+                .background(R.color.background_1)
+                .backgroundDark(R.color.background_dark_1)
+                .build());
+
+
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.title_2)
+                .description(R.string.description_2)
+                .background(R.color.background_2)
+                .backgroundDark(R.color.background_dark_2)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.title_3)
+                .description(R.string.description_3)
+                .background(R.color.background_2)
+                .backgroundDark(R.color.background_dark_2)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.title_4)
+                .description(R.string.description_4)
+                .background(R.color.background_2)
+                .backgroundDark(R.color.background_dark_2)
+                .build());
+
+
+    }
+}
