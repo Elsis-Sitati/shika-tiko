997e35ce5b2ec184bdbd9b17c4533115708c62b2
Elsis-Sitati <sitatielsis@gmail.com>
2016-03-09 09:23:36 +0300

    Fix splash screen bug

--- app/src/main/java/com/example/elsis/shikatiko/Splash.java
+++ app/src/main/java/com/example/elsis/shikatiko/Splash.java
@@ -1,13 +1,63 @@
 package com.example.elsis.shikatiko;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.github.jlmd.animatedcircleloadingview.AnimatedCircleLoadingView;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends AppCompatActivity  {
 
+    private AnimatedCircleLoadingView animatedCircleLoadingView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        animatedCircleLoadingView = (AnimatedCircleLoadingView) findViewById(R.id.circle_loading_view);
+        startLoading();
+        startPercentMockThread();
+        animatedCircleLoadingView.startDeterminate();
     }
+    private void startLoading() {
+        animatedCircleLoadingView.startDeterminate();
+    }
+
+    private void startPercentMockThread() {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1500);
+                    for (int i = 0; i <= 100; i++) {
+                        Thread.sleep(65);
+                        changePercent(i);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+    private void changePercent(final int percent) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                animatedCircleLoadingView.setPercent(percent);
+            }
+        });
+    }
+
+    public void resetLoading() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                animatedCircleLoadingView.resetLoading();
+            }
+        });
+    }
+
+
 }
+
+
